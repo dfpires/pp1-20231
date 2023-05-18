301b0dcb1ef3afc620522f028e4a2f5d655a9ff6
Daniel Pires <dfpires@gmail.com>
2023-05-18 19:32:32 -0300

    Add files via upload

--- social-media/frontend/scripts.js
+++ social-media/frontend/scripts.js
@@ -15,3 +15,29 @@ async function consultaPosts(){
     document.getElementById("corpoTabela").innerHTML = conteudoTabela
 
 }
+
+// consome que api que cadastra um post no banco de dados
+async function confirmar(){
+    // recupera os dados do formulário
+    const title = document.getElementById("title").value
+    const content = document.getElementById("content").value
+    const published = document.getElementById("sim").checked
+    alert(published)
+    const corpo = {title, content, published}
+    // chama a api
+    const post = await fetch('http://localhost:3333/post', {
+        method: 'POST',
+        body: JSON.stringify(corpo), // JSON transformado em string
+        headers: {
+            "Content-Type": "application/json;charset=UTF-8"
+        }
+    })
+    .then( resposta => {
+        alert('Operação realizada com sucesso')
+    })
+    .catch(error => {
+        alert('Operação falhou')
+    })
+    // atualiza a tabela
+    consultaPosts()
+}
