971e487d08c734fe93d01b2e2103ae26a63b8a3c
Daniel Pires <dfpires@gmail.com>
2023-06-07 20:58:11 -0300

    Add files via upload

--- social-media/frontend/scripts.js
+++ social-media/frontend/scripts.js
@@ -1,6 +1,9 @@
 async function consultaPosts(){
     // consome a API e guarda o resultado em posts
-    const posts = await fetch('http://localhost:3333/posts')
+    // recupera o cookie do id do usuário
+    let aux = document.cookie.split('=')
+    let userId = Number(aux[1])
+    const posts = await fetch(`http://localhost:3333/posts/user/${userId}`)
                         .then(resposta => {
                             return resposta.json()
                         })
@@ -9,18 +12,19 @@ async function consultaPosts(){
     // percorre cada post presente em posts e montar o conteúdo da tabela
     posts.forEach( post => {
         // acumula na variável conteudoTabela os dados de cada post
-        conteudoTabela += `<tr> <td> ${post.id} </td> <td> ${post.title} </td> <td> ${post.content} </td> <td> ${post.published} </td> <td> <button onclick="remover(${post.id})"> <i class="bi bi-trash"></i> </button> </td> <td> <button onclick="editar(${post.id}, '${post.title}', '${post.content}', ${post.published})">  <i class="bi bi-pencil"></i> </button> </td> </tr>`
+        conteudoTabela += `<tr> <td> ${post.id} </td> <td> ${post.title} </td> <td> ${post.content} </td> <td> ${post.likes} </td> <td> ${post.published} </td> <td> <button onclick="remover(${post.id})"> <i class="bi bi-trash"></i> </button> </td> <td> <button onclick="editar(${post.id}, '${post.title}', '${post.content}', ${post.likes}, ${post.published})">  <i class="bi bi-pencil"></i> </button> </td> </tr>`
     })
     // vamos jogar os dados no HTML
     document.getElementById("corpoTabela").innerHTML = conteudoTabela
 
 }
 
-function editar(id, title, content, published){
+function editar(id, title, content, likes, published){
     // alimenta o formulário
     document.getElementById("id").value = id
     document.getElementById("title").value = title;
     document.getElementById("content").value = content;
+    document.getElementById("likes").value = likes;
     (published) ? document.getElementById("sim").checked = true : 
                   document.getElementById("nao").checked = true
 }
@@ -48,22 +52,25 @@ async function confirmar(){
     // recupera os dados do formulário
     const title = document.getElementById("title").value
     const content = document.getElementById("content").value
+    const likes = Number(document.getElementById("likes").value)
     const published = document.getElementById("sim").checked
+    // o id será vazio se for inserção e terá conteúdo se for atualização
     const id = Number(document.getElementById("id").value)
    
     let corpo
     let verbo
     if (id) { // atualizar
-        corpo = {id, title, content, published}
+        corpo = {id, title, content, likes, published}
         verbo = 'PUT'
     }
     else { // cadastrar
-        corpo = {title, content, published}
+        // recupera o cookie
+        let aux = document.cookie.split('=')
+        let userId = Number(aux[1])
+        corpo = {title, content, likes, published, userId}
         verbo = 'POST'
     }
      
-    console.log(corpo)
-    console.log(JSON.stringify(corpo))
     // chama a api
     const post = await fetch('http://localhost:3333/post', {
         method: verbo,
@@ -73,9 +80,7 @@ async function confirmar(){
         }
     })
     .then( resposta => {
-        alert(verbo)
-        alert(`${corpo.id} - ${corpo.title} - ${corpo.content} - ${corpo.published}`)
-        alert('Operação realizada com sucesso')
+       alert('Operação realizada com sucesso')
     })
     .catch(error => {
         alert('Operação falhou')
@@ -86,6 +91,111 @@ async function confirmar(){
     document.getElementById("id").value = ''
     document.getElementById("title").value = ''
     document.getElementById("content").value = ''
+    document.getElementById("likes").value = '0'
     document.getElementById("sim").checked = false
     document.getElementById("nao").checked = false
 }
+
+async function entrar(){
+        // recupera dados do form
+        const username = document.getElementById("username").value
+        const password = document.getElementById("password").value
+        const corpo = {username, password}
+        let resposta = await fetch(`http://localhost:3333/user/verifica`, {
+            method: 'POST',
+            body: JSON.stringify(corpo),
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        })
+        .then(resp => {
+                return resp.json()
+        })
+        .catch(error => {
+                alert(`Erro na execução ${error}`)
+                exit(0) 
+        })
+        if (resposta == null){
+            alert('Usuário/Senha não existem')
+        }
+        else {
+            // guarda no cookie uma variável chamada id contendo o id do usuário
+            document.cookie = `id=${resposta.id}`
+            // vai direcionar o usuário para a tela principal do sistema
+            // vamos usar a extensão live server
+            window.open(`http://127.0.0.1:5500/frontend/post.html`);   
+        }
+       
+    }
+
+    async function montarSelect(){
+        // recupera valor do cookie
+        let aux = document.cookie.split('=')
+        let userId = Number(aux[1])
+        const posts = await fetch(`http://localhost:3333/posts/user/${userId}`)
+            .then(resposta => {
+               return resposta.json()
+            })
+            .catch(error => {
+                alert('Não carregou os posts')
+            })
+         // alimenta o select
+         let conteudoSelect = '' 
+         posts.forEach( post => {
+            conteudoSelect += `<option value='${post.id}'> ${post.title} </option>`
+         })
+         document.getElementById("post").innerHTML = conteudoSelect
+    }
+
+    async function gostar(){
+        // recupera os dados do formulário
+        const id = Number(document.getElementById("post").value) // recupera o id do post
+        const x = Number(document.getElementById("quantity").value)
+        // cria objeto para enviar
+        const objeto = {id, x}
+        // consumir a API
+        const resultado = await fetch('http://localhost:3333/post/gostar', {
+            method: 'PATCH',
+            body: JSON.stringify(objeto),
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        })
+        .then(resp => {
+            // alert(resultado.status) ou resultado.data.status
+            alert('Like realizada com sucesso')
+        })
+        .catch(error => {
+            // alert(resultado.status) ou resultado.data.status
+            alert('Problema no like')
+        })
+    }
+
+    async function desgostar(){
+        // recupera os dados do formulário
+        const id = Number(document.getElementById("post").value) // recupera o id do post
+        const x = Number(document.getElementById("quantity").value)
+        // cria objeto para enviar
+        const objeto = {id, x}
+        // consumir a API
+        const resultado = await fetch('http://localhost:3333/post/desgostar', {
+            method: 'PATCH',
+            body: JSON.stringify(objeto),
+            headers: {
+                'Content-Type': 'application/json;charset=UTF-8'
+            }
+        })
+        .then(resp => {
+             return resp.json()
+        })
+        .catch(error => {
+            // alert(resultado.status) ou resultado.data.status
+            alert('Problema na retirada de like')
+        })
+        if (resultado.count == 0){ // não tem likes suficientes para desgostar
+            alert('Likes insuficientes')
+        }
+        else {
+            alert('Likes retirados com sucesso')
+        }
+    }
